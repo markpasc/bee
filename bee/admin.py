a3cb27beacd4f2b850549d18be0142e30e8fa8ec
Mark Paschal <markpasc@markpasc.org>
2011-05-18 19:19:56 -0700

    Improve posts list in the admin

--- bee/admin.py
+++ bee/admin.py
@@ -8,8 +8,11 @@ admin.site.register(TrustGroup)
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'slug', 'created')
+    list_display = ('title', 'author', 'slug', 'atom_id', 'private', 'published')
+    list_display_links = ('title', 'slug')
+    list_filter = ('author',)
     prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'slug', 'html')
 
 admin.site.register(Post, PostAdmin)
 
