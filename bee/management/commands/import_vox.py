7048b6cf32f1a0ec1f4d3a56d219a67d81fd324c
Mark Paschal <markpasc@markpasc.org>
2011-07-10 18:17:22 -0700

    Import Vox posts (plus referenced images) as a management command

--- bee/management/commands/import_vox.py
+++ bee/management/commands/import_vox.py
@@ -1,173 +1,256 @@
-#!/usr/bin/env python
-
 from datetime import datetime
+from itertools import ifilterfalse
 import logging
+from optparse import make_option
+import os
+from os.path import dirname, join, basename
+import random
+import re
+import string
 import sys
 from xml.etree import ElementTree
 
-import argparse
+from BeautifulSoup import BeautifulSoup, NavigableString
 from django.contrib.auth.models import User
+from django.core.files import File
+from django.core.management.base import BaseCommand, CommandError
+from django.template.defaultfilters import slugify, striptags
+from django.utils.text import truncate_words
 
-from bee.models import Post, Identity, TrustGroup
+import bee.models
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
+class Command(BaseCommand):
 
-    parser = argparse.ArgumentParser(description='Import posts from a Vox Atom XML export.')
-    parser.add_argument('source', metavar='FILE', help='The filename of the XML export (or - for stdin)')
-    parser.add_argument('--openid', metavar='URL', required=True,
-        help='Your Vox OpenID for linking your posts and comments to you')
-    parser.add_argument('-v', dest='verbosity', action='append_const', const=1,
-        help='Be more verbose (stackable)', default=[2])
-    parser.add_argument('-q', dest='verbosity', action='append_const', const=-1,
-        help='Be less verbose (stackable)')
+    args = '<export file>'
+    help = 'Import posts from a Vox XML export.'
+    option_list = BaseCommand.option_list + (
+        make_option('--openid',
+            metavar='URL',
+            help='Your Vox OpenID for linking your posts and comments to you',
+        ),
+    )
 
-    args = parser.parse_args(argv)
+    def handle(self, source, **options):
+        openid = options.get('openid')
+        if not openid:
+            raise ValueError("Argument --openid is required")
+        self.make_my_openid(openid)
 
-    verbosity = sum(args.verbosity)
-    verbosity = 0 if verbosity < 0 else verbosity if verbosity < 4 else 4
-    log_level = [logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG][verbosity]
-    logging.getLogger().setLevel(level=log_level)
-    logging.getLogger('django.db').setLevel(level=logging.ERROR)
-    logging.info('Set log level to %s', logging.getLevelName(log_level))
+        self.sourcepath = dirname(source)
+        tree = ElementTree.parse(source)
 
-    # Rectify my OpenID first.
-    openid = args.openid
-    person = User.objects.all().order_by('id')[0]
-    try:
-        ident = Identity.objects.get(identifier=openid)
-    except Identity.DoesNotExist:
-        logging.info('Creating new identity mapping to %s for %s', person.username, openid)
-        ident = Identity(identifier=openid, user=person)
-        ident.save()
-    else:
-        if ident.user.pk == person.pk:
-            logging.debug('Identity %s is already yours, yay', openid)
+        self.import_assets(tree)
+        self.import_comments(tree)
+
+    def person_for_openid(self, openid, display_name):
+        ident_obj, created = bee.models.Identity.objects.get_or_create(identifier=openid)
+        return ident_obj
+
+    def make_my_openid(self, openid):
+        # Rectify my OpenID first.
+        person = User.objects.all().order_by('id')[0]
+        try:
+            ident = bee.models.Identity.objects.get(identifier=openid)
+        except bee.models.Identity.DoesNotExist:
+            logging.info('Creating new identity mapping to %s for %s', person.username, openid)
+            ident = bee.models.Identity(identifier=openid, user=person)
+            ident.save()
         else:
-            logging.info('Merging existing person %s for identity %s into person %s',
-                ident.user.username, openid, person.username)
-            ident.user.merge_into(person)
+            if ident.user.pk == person.pk:
+                logging.debug('Identity %s is already yours, yay', openid)
+            else:
+                logging.info('Merging existing person %s for identity %s into person %s',
+                    ident.user.username, openid, person.username)
+                ident.user.merge_into(person)
 
-    import_assets(sys.stdin if args.source == '-' else args.source)
+    def basic_asset_for_element(self, asset_el):
+        atom_id = asset_el.findtext('{http://www.w3.org/2005/Atom}id')
+        logging.debug('Parsing asset %s', atom_id)
 
-    return 0
+        try:
+            asset = bee.models.Post.objects.get(atom_id=atom_id)
+        except bee.models.Post.DoesNotExist:
+            asset = bee.models.Post(atom_id=atom_id)
 
+        publ = asset_el.findtext('{http://www.w3.org/2005/Atom}published')
+        publ_dt = datetime.strptime(publ, '%Y-%m-%dT%H:%M:%SZ')
+        asset.published = publ_dt
 
-def person_for_openid(openid, display_name):
-    ident_obj, created = Identity.objects.get_or_create(identifier=openid)
-    return ident_obj
-
-
-def basic_asset_for_element(asset_el):
-    atom_id = asset_el.findtext('{http://www.w3.org/2005/Atom}id')
-    logging.debug('Parsing asset %s', atom_id)
-
-    try:
-        asset = Post.objects.get(atom_id=atom_id)
-    except Post.DoesNotExist:
-        asset = Post(atom_id=atom_id)
-    asset.imported = True
-
-    publ = asset_el.findtext('{http://www.w3.org/2005/Atom}published')
-    publ_dt = datetime.strptime(publ, '%Y-%m-%dT%H:%M:%SZ')
-    asset.created = publ_dt
-
-    content_el = asset_el.find('{http://www.w3.org/2005/Atom}content')
-    content_type = content_el.get('type')
-    if content_type == 'html':
-        asset.html = content_el.text
-    elif content_type == 'xhtml':
-        html_el = content_el.find('{http://www.w3.org/1999/xhtml}div')
-        html = html_el.text or u''
-        html += u''.join(ElementTree.tostring(el) for el in html_el.getchildren())
+        content_el = asset_el.find('{http://www.w3.org/2005/Atom}content')
+        if content_el is None:
+            html = ''
+        else:
+            content_type = content_el.get('type')
+            if content_type == 'html':
+                html = content_el.text
+            elif content_type == 'xhtml':
+                html_el = content_el.find('{http://www.w3.org/1999/xhtml}div')
+                html = html_el.text or u''
+                html += u''.join(ElementTree.tostring(el) for el in html_el.getchildren())
         asset.html = html
 
-    author_el = asset_el.find('{http://www.w3.org/2005/Atom}author')
-    author_name = author_el.findtext('{http://www.w3.org/2005/Atom}name')
-    openid = author_el.findtext('{http://www.w3.org/2005/Atom}uri')
-    # Import "gone" folks' comments anonymously.
-    if openid != 'http://www.vox.com/gone/':
-        asset.author = person_for_openid(openid, author_name).user
+        author_el = asset_el.find('{http://www.w3.org/2005/Atom}author')
+        author_name = author_el.findtext('{http://www.w3.org/2005/Atom}name')
+        openid = author_el.findtext('{http://www.w3.org/2005/Atom}uri')
+        # Import "gone" folks' comments anonymously.
+        if openid != 'http://www.vox.com/gone/':
+            asset.author = self.person_for_openid(openid, author_name).user
 
-    return asset
+        if not asset.slug:
+            atom_links = asset_el.findall('{http://www.w3.org/2005/Atom}link')
+            permalinks = [el for el in atom_links if el.get('rel') == 'alternate' and el.get('type') == 'text/html']
+            if not permalinks:
+                raise ValueError("Could not find text/html alternate link for post %r" % atom_id)
+            vox_url = permalinks[0].get('href')
+            mo = re.search(r'/(?P<slug>[^/\.]+)\.html', vox_url)
+            if mo is None:
+                raise ValueError("Could not find slug in Vox post URL %r" % vox_url)
+            vox_slug = mo.group('slug')
 
+            def gunk_slugs():
+                chars = string.letters + string.digits + string.digits
+                while True:
+                    gunk = ''.join(random.choice(chars) for i in range(7))
+                    yield gunk
 
-def import_assets(source):
-    tree = ElementTree.parse(source)
+            def possible_slugs():
+                slug_source = vox_slug  # should always have one?
+                if not slug_source:
+                    slug_source = asset.title
+                if not slug_source:
+                    post_text = striptags(asset.html)
+                    slug_source = truncate_words(post_test, 7, end_text='')
+                if not slug_source:
+                    for gunk in gunk_slugs():
+                        yield gunk
+                yield slugify(slug_source)
+                for gunk in gunk_slugs():
+                    possible_unique = '%s %s' % (slug_source, gunk)
+                    yield slugify(possible_unique)
 
-    groups = dict()
+            other_posts = asset.author.posts_authored.all()
+            if asset.id:
+                other_posts = other_posts.exclude(id=asset.id)
+            def is_slug_used(slug):
+                return other_posts.filter(slug=slug).exists()
 
-    for asset_el in tree.findall('{http://www.w3.org/2005/Atom}entry'):
-        # Skip comments this go-round.
-        if asset_el.find('{http://purl.org/syndication/thread/1.0}in-reply-to') is not None:
-            continue
+            unused_slugs = ifilterfalse(is_slug_used, possible_slugs())
+            asset.slug = unused_slugs.next()  # only need the first unused
 
-        asset = basic_asset_for_element(asset_el)
+        return asset
 
-        asset.title = asset_el.findtext('{http://www.w3.org/2005/Atom}title')
+    def import_image_assets(self, content_root, author, post_created):
+        assets = list()
 
-        asset.save()
-        logging.info('Saved new asset %s (%s) as #%d', asset.atom_id, asset.title, asset.pk)
+        for el in content_root.findAll('img'):
+            img_src = el.get('src')
+            mo = re.match(r'http://a\d+\.vox\.com/(?P<asset_id>6a\w+)', img_src)
+            if mo is None:
+                continue
+            asset_id = mo.group('asset_id')
 
-        asset_groups = list()
-        privacies = asset_el.findall('{http://www.sixapart.com/ns/atom/privacy}privacy/{http://www.sixapart.com/ns/atom/privacy}allow')
-        private_group = TrustGroup.objects.get(tag='private')
-        for privacy in privacies:
-            assert privacy.get('policy') == 'http://www.sixapart.com/ns/atom/permissions#read', 'Privacy policy for post is not about reading :('
-            group_ref = privacy.get('ref')
+            for ext in ('gif', 'jpg'):
+                img_path = join(self.sourcepath, 'assets', asset_id + '-pi.' + ext)
+                if not os.access(img_path, os.R_OK):
+                    logging.warn("Couldn't import asset for URL %s: file %s doesn't exist", img_src, img_path)
+                    continue
 
-            # Ignore "everyone" since that's the absence of a group.
-            if group_ref == 'http://www.sixapart.com/ns/atom/groups#everyone':
-                # Ignore this one.
+                with open(img_path, 'r') as f:
+                    asset = bee.models.Asset(author=author, created=post_created)
+                    asset.sourcefile.save(basename(img_path), File(f), save=True)
+                    assets.append(asset)
+
+                    el['src'] = asset.sourcefile.url  # guess this is a property?
+                    logging.debug("Importing asset for URL %s (will now be at %s)", img_src, el['src'])
+
+        return assets
+
+    def import_assets(self, tree):
+        groups = dict()
+
+        for asset_el in tree.findall('{http://www.w3.org/2005/Atom}entry'):
+            # Skip comments this go-round.
+            if asset_el.find('{http://purl.org/syndication/thread/1.0}in-reply-to') is not None:
                 continue
 
-            # Use the special private group for "self".
-            if group_ref == 'http://www.sixapart.com/ns/atom/groups#self':
-                asset_groups.append(private_group)
+            # Skip anything that isn't a post.
+            atom_links = asset_el.findall('{http://www.w3.org/2005/Atom}link')
+            permalinks = [el for el in atom_links if el.get('rel') == 'alternate' and el.get('type') == 'text/html']
+            if not permalinks:
+                raise ValueError("Could not find text/html alternate link for post %r" % atom_id)
+            vox_url = permalinks[0].get('href')
+            if re.search(r'/library/post/', vox_url) is None:
                 continue
 
-            try:
-                group = groups[group_ref]
-            except KeyError:
-                group, created = TrustGroup.objects.get_or_create(tag=group_ref,
-                    defaults={'display_name': privacy.get('name')})
-                groups[group_ref] = group
-            asset_groups.append(group)
+            asset = self.basic_asset_for_element(asset_el)
+            asset.title = asset_el.findtext('{http://www.w3.org/2005/Atom}title')
 
-        logging.debug('Assigning asset %s to %d groups', asset.atom_id, len(asset_groups))
-        asset.private_to = asset_groups
+            content_root = BeautifulSoup(asset.html)
+            image_assets = self.import_image_assets(content_root, asset.author, asset.published)
+            asset.html = str(content_root)
 
-    for comment_el in tree.findall('{http://www.w3.org/2005/Atom}entry'):
+            logging.info("Saving asset with author ID %d and slug '%s'", asset.author_id, asset.slug)
+            asset.save()
+            logging.info('Saved new asset %s (%s) as #%d', asset.atom_id, asset.title, asset.pk)
+
+            for image_asset in image_assets:
+                image_asset.posts.add(asset)
+
+            asset_groups = list()
+            privacies = asset_el.findall('{http://www.sixapart.com/ns/atom/privacy}privacy/{http://www.sixapart.com/ns/atom/privacy}allow')
+            for privacy in privacies:
+                assert privacy.get('policy') == 'http://www.sixapart.com/ns/atom/permissions#read', 'Privacy policy for post is not about reading :('
+                group_ref = privacy.get('ref')
+
+                # Ignore "everyone" since that's the absence of a group.
+                if group_ref == 'http://www.sixapart.com/ns/atom/groups#everyone':
+                    # Ignore this one.
+                    continue
+
+                # Use the special private group for "self".
+                if group_ref == 'http://www.sixapart.com/ns/atom/groups#self':
+                    asset.private = True
+                    continue
+
+                try:
+                    group = groups[group_ref]
+                except KeyError:
+                    group, created = bee.models.TrustGroup.objects.get_or_create(tag=group_ref,
+                        defaults={'user': asset.author, 'display_name': privacy.get('name')})
+                    groups[group_ref] = group
+                asset.private = True
+                asset_groups.append(group)
+
+            logging.debug('Assigning asset %s to %d groups', asset.atom_id, len(asset_groups))
+            asset.private_to = asset_groups
+
+    def import_comments(self, tree):
         # TODO: deal with comments again
-        break
+        return
 
-        # This time, *only* comments.
-        reply_el = asset_el.find('{http://purl.org/syndication/thread/1.0}in-reply-to')
-        if reply_el is None:
-            continue
+        for comment_el in tree.findall('{http://www.w3.org/2005/Atom}entry'):
+            # This time, *only* comments.
+            reply_el = asset_el.find('{http://purl.org/syndication/thread/1.0}in-reply-to')
+            if reply_el is None:
+                continue
 
-        parent_ref = reply_el.get('ref')
-        try:
-            parent_asset = Post.objects.get(atom_id=parent_ref)
-        except Post.DoesNotExist:
-            # OOPS
-            logging.warn('Referenced parent asset %s does not exist; skipping comment :(', reply_el.get('href'))
-            continue
+            parent_ref = reply_el.get('ref')
+            try:
+                parent_asset = bee.models.Post.objects.get(atom_id=parent_ref)
+            except bee.models.Post.DoesNotExist:
+                # OOPS
+                logging.warn('Referenced parent asset %s does not exist; skipping comment :(', reply_el.get('href'))
+                continue
 
-        asset = basic_asset_for_element(comment_el)
+            asset = basic_asset_for_element(comment_el)
 
-        asset.in_reply_to = parent_asset
-        asset.in_thread_of = parent_asset.in_thread_of or parent_asset
+            asset.in_reply_to = parent_asset
+            asset.in_thread_of = parent_asset.in_thread_of or parent_asset
 
-        asset.save()
+            asset.save()
 
-        asset.private_to = parent_asset.private_to.all()
-
-
-    # TODO: import friends?
-    # TODO: import favorites :(
+            asset.private_to = parent_asset.private_to.all()
 
 
 if __name__ == '__main__':
