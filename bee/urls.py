3b45b45fac455843e077e08322945e714bbd25c1
Mark Paschal <markpasc@markpasc.org>
2011-07-12 23:19:38 -0700

    Link to the feed from the index page

--- bee/urls.py
+++ bee/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('bee.views',
     url(r'^$', 'index', name='index'),
-    url(r'^feed/$', 'feed'),
+    url(r'^feed/$', 'feed', name='feed'),
     url(r'^_/editor', 'editor', name='editor'),
     url(r'^_/edit', 'edit'),
 
