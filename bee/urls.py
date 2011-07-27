5f47d16924a17a8ea31b5be0899dc87dcb9afbac
Mark Paschal <markpasc@markpasc.org>
2011-07-27 15:14:52 -0700

    Use django-social-auth for signing in

--- bee/urls.py
+++ bee/urls.py
@@ -12,3 +12,8 @@ urlpatterns = patterns('bee.views',
     url(r'^_/edit$', 'edit'),
     url(r'^_/comments/', include('django.contrib.comments.urls')),
 )
+
+urlpatterns += patterns('django.contrib.auth.views',
+    url(r'^signin/$', 'login', {'template_name': 'accounts/signin.html'}, name='signin'),
+    url(r'^signout/$', 'logout', {'template_name': 'accounts/signed_out.html'}, name='signout'),
+)
