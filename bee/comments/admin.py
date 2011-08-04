03262e5e05fb5851c7343a57a5c76623332c20b6
Mark Paschal <markpasc@markpasc.org>
2011-08-04 16:52:14 -0700

    Try tweaking comment list display this way

--- bee/comments/admin.py
+++ bee/comments/admin.py
@@ -13,4 +13,15 @@ class PostCommentAdmin(CommentsAdmin):
         ),
     )
 
+    list_display = ('comment', 'name', 'is_user', 'content_object', 'ip_address', 'submit_date', 'is_visible')
+
+    def is_visible(self, obj):
+        return obj.is_public and not obj.is_removed
+    is_visible.boolean = True
+
+    def is_user(self, obj):
+        return bool(obj.user_id)
+    is_user.boolean = True
+
+
 admin.site.register(PostComment, PostCommentAdmin)
